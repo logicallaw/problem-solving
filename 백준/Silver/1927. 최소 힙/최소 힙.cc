51fb5de4833aa5042ca306cd510c77eac5316d8a
Junho Kim <93714449+logicallaw@users.noreply.github.com>
2025-12-06 11:44:41 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    priority_queue<int> pq;
+    for(int i{ 0 }; i < n; i++) {
+        int x;
+        cin >> x;
+        if(x == 0) {
+            if(pq.empty()) {
+//                cout << 0 << endl;
+                printf("%d\n", 0);
+            } else {
+                int min = pq.top();
+                printf("%d\n", -min);
+                pq.pop();
+            }
+        } else {
+            pq.push(-x);
+        }
+    }
+}
