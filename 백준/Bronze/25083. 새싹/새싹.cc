1af66f2f0a396e03e2591da3b652b206a21af21a
Junho Kim <93714449+logicallaw@users.noreply.github.com>
2025-12-13 16:29:19 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,23 @@
+/*
+         ,r'"7
+r`-_   ,'  ,/
+ \. ". L_r'
+   `~\/
+      |
+      |
+
+*/
+#include <iostream>
+using namespace std;
+int main(void)
+{ 
+    cout << "         ,r\'\"7" << endl;
+    cout << "r`-_   ,\'  ,/" << endl;
+    cout << " \\. \". L_r\'" << endl;
+    cout << "   `~\\/" << endl;
+    cout << "      |" << endl;
+    cout << "      |" << endl;
+
+    return 0;
+
+}
